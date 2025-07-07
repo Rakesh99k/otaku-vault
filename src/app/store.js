c07b278770070f9812f44c7fb70284a541c807eb
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2025-07-07 23:59:13 +0530

    added watchlistSlice.js to store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import watchlistReducer from '../features/watchlistSlice';
 
 export const store = configureStore({
   reducer: {
-    // slices will go here later
+    watchlist: watchlistReducer,
   },
 });
