f7470e43aff4171592bacb774c7ff296f8488753
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2025-07-23 14:39:35 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import logo from '../assets/logo_otaku-vault.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="navbar-logo">OtakuVault</h1>
+      <div className="navbar-logo-wrapper">
+        <img src={logo} alt="OtakuVault Logo" className="navbar-logo-img" />
+        <h1 className="navbar-logo">OtakuVault</h1>
+      </div>
       <div className="navbar-links">
         <Link to="/?reset=true">Home</Link>
         <Link to="/watchlist">Watchlist</Link>
